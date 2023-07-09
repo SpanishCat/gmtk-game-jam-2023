e9f0b833eeaeea15d98398efa968b5df9d1a34b8
SpanishCat <73846269+SpanishCat@users.noreply.github.com>
2023-07-09 19:40:16 +0300

    Placement UI

--- JamJam3/Assets/Scripts/ButtonFunctions.cs
+++ JamJam3/Assets/Scripts/ButtonFunctions.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonFunctions : MonoBehaviour
 {
+    // Main Menu
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -12,5 +14,41 @@ public class ButtonFunctions : MonoBehaviour
     {
         Debug.Log("Exited Game");
         Application.Quit();
+    }
+
+    // Troop Placement
+    public enum Target
+    {
+        Towers,
+        Finishline
+    }
+
+    //public class TroopsOrder
+    //{
+    GameObject type;
+    public short amount;
+    //public Target target;
+    public string target;
+
+    public void SetType(GameObject troopType) { type = troopType; }
+    public void SetAmount(short troopAmount) { amount = troopAmount; }
+    public void SetTarget(string troopTarget) { target = troopTarget; }
+    //}
+
+    public void UpdateTarget()
+    {
+        Dropdown dropdown = GetComponent<Dropdown>();
+
+        if (dropdown.itemText.text == "Attack Towers")
+            SetTarget("Towers");
+        else
+            SetTarget("Finishline");
+    }
+
+    public void SendTroops()
+    {
+        Debug.Log("Sending " + amount + 
+            " " + type.name + 
+            "s for target: " + target);
     }
 }
